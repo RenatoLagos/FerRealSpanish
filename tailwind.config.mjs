bb733e87acb14e75fa716fe9240f99ffc17618f2
RenatoLagos <renato.lagos.a@gmail.com>
2025-05-13 19:04:43 -0300

    Refactor Tailwind configuration, enhance UI components, and add new sections. Removed unused color definitions and border radius settings from Tailwind config. Introduced new components: FullProgram, CallToAction, QuoteSection, and ProgramsSection. Updated Hero and Features components for improved styling and content. Integrated new font styles for better typography.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,31 +3,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      colors: {
-        'warm-orange': 'hsl(var(--primary))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        card: 'hsl(var(--card))',
-        'card-foreground': 'hsl(var(--card-foreground))',
-        primary: 'hsl(var(--primary))',
-        'primary-foreground': 'hsl(var(--primary-foreground))',
-        secondary: 'hsl(var(--secondary))',
-        'secondary-foreground': 'hsl(var(--secondary-foreground))',
-        accent: 'hsl(var(--accent))',
-        'accent-foreground': 'hsl(var(--accent-foreground))',
-        muted: 'hsl(var(--muted))',
-        'muted-foreground': 'hsl(var(--muted-foreground))',
-        destructive: 'hsl(var(--destructive))',
-        'destructive-foreground': 'hsl(var(--destructive-foreground))',
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-      },
-      borderRadius: {
-        DEFAULT: 'var(--radius)',
-      },
       fontFamily: {
         nunito: ['Nunito', 'sans-serif'],
+        satoshi: ['Satoshi', 'sans-serif']
       },
     },
   },
